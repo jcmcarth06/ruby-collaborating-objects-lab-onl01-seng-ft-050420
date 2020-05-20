a5ff9a174b6268198f8ba684328407e0a1b91105
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-20 01:35:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,33 +6,38 @@ class Song
 
   def initialize(name)
     @name = name
-    songs = []
-    self.save
-  end
-
-  def add_song(song)
-    self.songs << self
-  end
-
-  def save
-    @@all << self
+  save
   end
 
   def self.all
     @@all
   end
 
+  def save
+    self.class.all << self
+  end
+
+  def add_song(song)
+    self.songs << self
+  end
+
+  def self.new_by_filename(filename)
+    song = filename.split(" - ")[1]
+    artist = filename.split(" - ")[0]
+    new.song = self.new(song)
+    new.song.artist_name = artist
+    new_song
+  end
+  
+
+
+
   def artist=(artist)
     @artist = artist
     add_song
   end
 
-  def self.new_by_filename(filename)
-    song_array = filename.split(" - ")
-    song_name = song_array[1]
-    song = self.new(song_name)
-    song
-  end
+  
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
