452e1da88451983a8a38591afeb8b5c2103b8442
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-20 01:47:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,33 +1,33 @@
 class Artist
   attr_accessor :name, :songs
-  @@all = []  
+  @@all = []
   def initialize(name)
     @name = name
     @songs = []
     self.save
   end
   def add_song(song)
-    self.songs << song    
-                          
+    self.songs << song
+
   end
   def save
-    @@all << self     
-                      
+    @@all << self
+
   end
-  def self.all      
+  def self.all
     @@all
   end
-  def self.find_or_create_by_name(name)   
+  def self.find_or_create_by_name(name)
     if @@all.find {|a| a.name == name}
     @@all.find {|a| a.name == name}
   else
     Artist.new(name)
   end
 end
-  def self.find(name)     
+  def self.find(name)
     self.all.detect { |artist| artist.name == name }
   end
-  def self.create(name)   
+  def self.create(name)
     artist = Artist.new(name)
     artist.save
     artist
