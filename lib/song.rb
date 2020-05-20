837aeb8fbaeac915cf37b3e986f0574883043168
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-20 01:49:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,21 @@
 class Song
   attr_accessor :name, :artist
+  
   @@all = []
+  
   def initialize(name)
     @name = name
     save
   end
+  
   def self.all
     @@all
   end
+  
   def save
     self.class.all << self
   end
+  
   def self.new_by_filename(file_name)
     song = file_name.split(" - ")[1]
     artist = file_name.split(" - ")[0]
@@ -18,8 +23,10 @@ class Song
     new_song.artist_name = artist
     new_song
   end
+  
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
+  
 end
